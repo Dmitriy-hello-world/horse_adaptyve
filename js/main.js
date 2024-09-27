96e27b64b65e102356e75a695fa8c30ecbaa0218
Dmitriy-hello-world <easportstng@gmail.com>
2024-09-27 10:47:56 +0300

    fixed bugs 0.2

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ window.addEventListener("DOMContentLoaded", () => {
 	const burger = document.querySelector("header img")
 	const mobileMenu = document.querySelector(".header__nav_mobile")
 	const hiddenElement = document.querySelector(".hidden")
+	const contentWrapper = document.querySelector(".content-wrapper")
 	let isMenuOpen = false
 
 	const openMenu = () => {
 		mobileMenu.classList.add("header__nav_mobile_active")
 		document.body.style.overflow = 'hidden'
 		hiddenElement.classList.add("hidden__active")
+		contentWrapper.classList.add("blur")
 		burger.setAttribute("src", "images/close.svg")
 		isMenuOpen = true
 	}
@@ -16,6 +18,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		mobileMenu.classList.remove("header__nav_mobile_active")
 		document.body.style.overflow = ''
 		hiddenElement.classList.remove("hidden__active")
+		contentWrapper.classList.remove("blur")
 		burger.setAttribute("src", "images/menu.svg")
 		isMenuOpen = false
 	}
